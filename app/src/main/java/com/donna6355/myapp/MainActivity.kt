c727f229a6f9dd1716555853d8d3d63e0db14416
donna6355 <donnajun1214@gmail.com>
2024-04-01 12:44:09 +0900

    [add] surround widget and change background color

--- app/src/main/java/com/donna6355/myapp/MainActivity.kt
+++ app/src/main/java/com/donna6355/myapp/MainActivity.kt
@@ -3,12 +3,14 @@ package com.donna6355.myapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.donna6355.myapp.ui.theme.MyAppTheme
 
@@ -28,10 +30,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Good Afternoon $name!",
-            modifier = modifier
-    )
+//    Box { //alt + enter to surround widget
+        Surface(color = Color.Yellow) {
+            Text(
+                text = "Good Afternoon $name!",
+                modifier = modifier
+            )
+        }
+//    }
 }
 
 @Preview(showBackground = true)
