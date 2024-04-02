b8f97ff55630e42dbbb384b29e4077c6817d45d9
donna6355 <donnajun1214@gmail.com>
2024-04-02 10:23:26 +0900

    [add] image and string resources

--- hbdCard/app/src/main/java/com/donna6355/hbdcard/MainActivity.kt
+++ hbdCard/app/src/main/java/com/donna6355/hbdcard/MainActivity.kt
@@ -3,13 +3,25 @@ package com.donna6355.hbdcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.donna6355.hbdcard.ui.theme.HbdCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_isaac),
+                        from= stringResource(R.string.from_donna),
+                        modifier = Modifier.padding(20.dp)
+                    )
                 }
             }
         }
@@ -30,17 +46,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message:String,from:String,modifier: Modifier = Modifier){
+    Column (
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ){
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 132.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize =32.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingImage(message:String, from:String, modifier: Modifier=Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.3f
+        )
+        GreetingText(message = message, from = from)
+    }
+}
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun BirthdayCardPreview() {
     HbdCardTheme {
-        Greeting("Android")
+        GreetingImage(message = "Happy Birthday Isaac",from="from. Donna")
     }
 }
