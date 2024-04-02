02112873bf2fce84fa9833c957d889359347d24a
donna6355 <donnajun1214@gmail.com>
2024-04-02 12:35:49 +0900

    [add] weight and color

--- hbdCard/app/src/main/java/com/donna6355/hbdcard/MainActivity.kt
+++ hbdCard/app/src/main/java/com/donna6355/hbdcard/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -108,14 +114,86 @@ fun Article(title:String, paraOne:String, paraTwo:String,modifier:Modifier=Modif
     }
 
 }
+
+@Composable
+fun CompletionCard(header:String, desc:String, modifier: Modifier=Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_task_completed) , contentDescription = null)
+        Text(
+            text = header,
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top=24.dp, bottom=8.dp)
+        )
+        Text(
+            text = desc,
+            fontSize = 16.sp
+        )
+    }
+}
+
+@Composable
+fun ComposeCard(label:String, body:String,backgroundColor:Color, modifier: Modifier= Modifier){
+    Column(
+        modifier = modifier.fillMaxSize()
+            .background(color = backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = label,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp),
+        )
+        Text(
+            text = body,
+            textAlign = TextAlign.Justify,
+        )
+
+    }
+}
+@Composable
+fun ComposeQuadrant(){
+    Column() {
+        Row(Modifier.weight(1f)) {
+            ComposeCard(
+                label = stringResource(R.string.text_composable),
+                body = stringResource(R.string.text_composable_body),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposeCard(
+                label = stringResource(R.string.image_composable),
+                body = stringResource(R.string.image_composable_body),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row (Modifier.weight(1f)){
+            ComposeCard(label = stringResource(R.string.row_composable),
+                body = stringResource(R.string.row_composable_body),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposeCard(
+                label = stringResource(R.string.column_composable),
+                body = stringResource(R.string.column_composable_boy),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+                )
+        }
+    }
+}
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun BirthdayCardPreview() {
     HbdCardTheme {
-        Article(
-            title = stringResource(R.string.jetpack_compose_tutorial),
-            paraOne = stringResource(R.string.jetpack_compose),
-            paraTwo = stringResource(R.string.jetpack_tutorial)
-        )
+        ComposeQuadrant()
     }
 }
