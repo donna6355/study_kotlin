493193f176febf99340f173e8de6024e668f78ba
donna6355 <donnajun1214@gmail.com>
2024-04-04 11:27:06 +0900

    [add] leading icon and scrollable

--- tipCalculator/app/src/main/java/com/donna6355/tipcalculator/MainActivity.kt
+++ tipCalculator/app/src/main/java/com/donna6355/tipcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.donna6355.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -16,7 +17,10 @@ import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
@@ -29,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -59,7 +64,8 @@ fun TipLayout() {
         modifier = Modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
-            .safeDrawingPadding(),
+            .safeDrawingPadding()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -80,6 +86,7 @@ fun TipLayout() {
             label = R.string.bill_amount,
             value = amountInput,
             onValueChange = { amountInput = it },
+            icon = R.drawable.money,
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
@@ -92,6 +99,7 @@ fun TipLayout() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Done
             ),
+            icon = R.drawable.percent,
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
@@ -127,6 +135,7 @@ private fun tipCalculate(
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
+    @DrawableRes icon: Int,
     value: String,
     onValueChange: (String) -> Unit,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default.copy(
@@ -136,6 +145,7 @@ fun EditNumberField(
     modifier: Modifier = Modifier,
 ) {
     TextField(
+        leadingIcon = { Icon(painter = painterResource(icon), contentDescription = null) },
         value = value,
         onValueChange = onValueChange,
         modifier = modifier,
