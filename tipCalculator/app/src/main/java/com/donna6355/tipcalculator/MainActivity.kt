f03eceea257f1ca0271249429e9be6ba51e270fc
donna6355 <donnajun1214@gmail.com>
2024-04-04 10:37:02 +0900

    [add] tip calculate with stateful composable

--- tipCalculator/app/src/main/java/com/donna6355/tipcalculator/MainActivity.kt
+++ tipCalculator/app/src/main/java/com/donna6355/tipcalculator/MainActivity.kt
@@ -7,17 +7,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.donna6355.tipcalculator.ui.theme.TIpCalculatorTheme
@@ -55,6 +63,11 @@ fun TipLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
@@ -67,6 +80,23 @@ private fun tipCaculate(bill: Double, tip: Double = 0.15): String {
     val tip: Double = bill * tip
     return NumberFormat.getCurrencyInstance().format(tip)
 
+}
+
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    val amount: Double = amountInput.toDoubleOrNull() ?: 0.0
+    val tip: String = tipCaculate(amount)
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        modifier = modifier,
+        label = { Text(text = stringResource(id = R.string.bill_amount)) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
 }
 
 @Preview(showSystemUi = true)
