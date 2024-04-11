650c31ee5d0781daf9fa2ef983cceb15cc3e4ca2
donna6355 <donnajun1214@gmail.com>
2024-04-11 13:33:08 +0900

    [add] simple animation and gradle dependency

--- affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
+++ affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
@@ -3,7 +3,12 @@ package com.donna6355.affirmationapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,20 +24,30 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.donna6355.affirmationapp.data.Datasource
@@ -139,45 +154,75 @@ fun LectureGrid(topics: List<Topic>, modifier: Modifier = Modifier) {
 
 @Composable
 fun LectureCard(topic: Topic, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.height(68.dp)) {
-        Row {
-            Image(
-                painter = painterResource(id = topic.image),
-                contentDescription = stringResource(topic.name),
-                modifier = modifier
-                    .width(68.dp)
-                    .fillMaxHeight()
-                    .padding(
-                        end = dimensionResource(R.dimen.padding_medium)
-                    ),
-                contentScale = ContentScale.Crop
-            )
-            Column {
-                Spacer(
-                    modifier = Modifier.height(
-                        dimensionResource(R.dimen.padding_medium)
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer,
+        label = "",
+    )
+    Card {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMedium
                     )
                 )
-                Text(
-                    text = LocalContext.current.getString(topic.name),
-                    style = MaterialTheme.typography.bodyMedium
+                .background(color = color)
+        ) {
+            Row(modifier = Modifier.height(68.dp)) {
+                Image(
+                    painter = painterResource(id = topic.image),
+                    contentDescription = stringResource(topic.name),
+                    modifier = modifier
+                        .width(68.dp)
+                        .fillMaxHeight()
+                        .padding(
+                            end = dimensionResource(R.dimen.padding_medium)
+                        ),
+                    contentScale = ContentScale.Crop
                 )
-                Spacer(
-                    modifier = Modifier.height(
-                        dimensionResource(R.dimen.padding_small)
-                    )
-                )
-                Row {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_grain),
-                        contentDescription = null
+                Column {
+                    Spacer(
+                        modifier = Modifier.height(
+                            dimensionResource(R.dimen.padding_medium)
+                        )
                     )
                     Text(
-                        text = topic.number.toString(),
-                        style = MaterialTheme.typography.labelMedium
+                        text = LocalContext.current.getString(topic.name),
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Spacer(
+                        modifier = Modifier.height(
+                            dimensionResource(R.dimen.padding_small)
+                        )
+                    )
+                    Row {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_grain),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = topic.number.toString(),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
+                }
+                IconButton(onClick = { expanded = !expanded }) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Filled.ExpandMore else Icons.Filled.ExpandLess,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.secondary
                     )
                 }
             }
+            if (expanded) Text(
+                text = "STUDY HARD",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
