140785946362b1286abe3ce87a18b55ae8481481
donna6355 <donnajun1214@gmail.com>
2024-04-11 12:52:51 +0900

    [add] material theme

--- affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
+++ affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,13 +20,17 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -101,16 +104,35 @@ fun LectureApp() {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        },
+        modifier = modifier
+    )
+}
+
 @Composable
 fun LectureGrid(topics: List<Topic>, modifier: Modifier = Modifier) {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.padding(8.dp)
-    ) {
-        items(topics) {
-            LectureCard(topic = it)
+    Scaffold(
+        topBar = { AppBar() }
+    ) { it ->
+        LazyVerticalGrid(
+            contentPadding = it,
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(8.dp)
+        ) {
+            items(topics) {
+                LectureCard(topic = it)
+            }
         }
     }
 }
@@ -125,16 +147,26 @@ fun LectureCard(topic: Topic, modifier: Modifier = Modifier) {
                 modifier = modifier
                     .width(68.dp)
                     .fillMaxHeight()
-                    .padding(end = 16.dp),
+                    .padding(
+                        end = dimensionResource(R.dimen.padding_medium)
+                    ),
                 contentScale = ContentScale.Crop
             )
             Column {
-                Spacer(modifier = Modifier.height(16.dp))
+                Spacer(
+                    modifier = Modifier.height(
+                        dimensionResource(R.dimen.padding_medium)
+                    )
+                )
                 Text(
                     text = LocalContext.current.getString(topic.name),
                     style = MaterialTheme.typography.bodyMedium
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(
+                    modifier = Modifier.height(
+                        dimensionResource(R.dimen.padding_small)
+                    )
+                )
                 Row {
                     Image(
                         painter = painterResource(id = R.drawable.ic_grain),
--- affirmationApp/app/src/main/java/com/donna6355/affirmationapp/ui/theme/Type.kt
+++ affirmationApp/app/src/main/java/com/donna6355/affirmationapp/ui/theme/Type.kt
@@ -2,10 +2,17 @@ package com.donna6355.affirmationapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.donna6355.affirmationapp.R
 
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -14,7 +21,17 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
