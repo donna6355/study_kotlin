223a4a7abe61f2de84b61314fdaa0478fb9124df
donna6355 <donnajun1214@gmail.com>
2024-04-11 11:29:05 +0900

    [add] lazyVerticalGrid

--- affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
+++ affirmationApp/app/src/main/java/com/donna6355/affirmationapp/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +34,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.donna6355.affirmationapp.data.Datasource
 import com.donna6355.affirmationapp.model.Affirmation
+import com.donna6355.affirmationapp.model.Topic
 import com.donna6355.affirmationapp.ui.theme.AffirmationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,8 +47,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AffirmationApp()
+//                    AffirmationApp()
+                    LectureApp()
                 }
+
             }
         }
     }
@@ -78,6 +90,62 @@ fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Mod
                 affirmation = it,
                 modifier = Modifier.padding(8.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun LectureApp() {
+    LectureGrid(
+        topics = Datasource().topics,
+    )
+}
+
+@Composable
+fun LectureGrid(topics: List<Topic>, modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(topics) {
+            LectureCard(topic = it)
+        }
+    }
+}
+
+@Composable
+fun LectureCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card(modifier = modifier.height(68.dp)) {
+        Row {
+            Image(
+                painter = painterResource(id = topic.image),
+                contentDescription = stringResource(topic.name),
+                modifier = modifier
+                    .width(68.dp)
+                    .fillMaxHeight()
+                    .padding(end = 16.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column {
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = LocalContext.current.getString(topic.name),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = topic.number.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
         }
     }
 }
