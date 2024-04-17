17cca738f8ac3103d7300011405b48fefab9ab7d
Donna <donnajun1214@gmail.com>
2024-04-17 15:31:09 +0900

    [add] data repository and di

--- marsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ marsPhotos/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
 import com.example.marsphotos.network.MarsApi
 import com.example.marsphotos.network.MarsApiService
 import kotlinx.coroutines.launch
@@ -51,7 +52,8 @@ class MarsViewModel : ViewModel() {
     fun getMarsPhotos() {
         viewModelScope.launch {
             try {
-                val listResult = MarsApi.retrofitService.getPhotos()
+                val marsPhotosRepository = NetworkMarsPhotosRepository()
+                val listResult = marsPhotosRepository.getMarsPhotos()
                 marsUiState = MarsUiState.Success("${listResult.size} Mars Photo retrieved")
             } catch (e: IOException) {
                 marsUiState = MarsUiState.Error
